383f01977e38c30319bc1730db73e5ac62ee1779
Mateo Flores <dragonnegro1231@gmail.com>
2023-06-14 19:32:03 -0500

    Plantilla inicial del proyecto

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 
-
 app.use(express.json());
-// extra packages
 
-// routes
 app.get('/', (req, res) => {
   res.send('jobs api');
 });
-
 
 const port = process.env.PORT || 3000;
 
